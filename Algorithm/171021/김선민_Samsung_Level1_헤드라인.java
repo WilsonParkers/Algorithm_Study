6cb1253189546eb7c80ed430a1ea432a829e3327
seonmin-kim <30610392+seonmin-kim@users.noreply.github.com>
2017-11-03 00:01:21 +0900

    Create 김선민_Samsung_Level1_헤드라인.java

---
+++ Algorithm/171021/김선민_Samsung_Level1_헤드라인.java
@@ -0,0 +1,11 @@
+package samsung_Level1;
+
+import java.util.Scanner;
+
+public class Samsung_Level1_headline {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();				// 테스트 문장
+		System.out.println(str.toUpperCase());	// 출력
+	}
+}
