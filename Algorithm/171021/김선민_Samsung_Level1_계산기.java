d85a336351e7c5e9b4e3f6d5222b6d3c2542da2a
seonmin-kim <30610392+seonmin-kim@users.noreply.github.com>
2017-11-02 23:57:49 +0900

    Create 김선민_Samsung_Level1_계산기.java

---
+++ Algorithm/171021/김선민_Samsung_Level1_계산기.java
@@ -0,0 +1,15 @@
+package samsung_Level1;
+
+import java.util.Scanner;
+
+public class Samsung_Level1_cal {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+	}
+}
