d15ddd88153dcd60ee5c74f53fcb59a79bc9658f
seonmin-kim <30610392+seonmin-kim@users.noreply.github.com>
2017-11-03 00:04:54 +0900

    Create 김선민_Samsung_Level1_스탬프.java

---
+++ Algorithm/171021/김선민_Samsung_Level1_스탬프.java
@@ -0,0 +1,13 @@
+package samsung_Level1;
+
+import java.util.Scanner;
+
+public class Samsung_Level1_stamp {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();				// 테스트 수
+		for(int i=0; i<n; i++){
+			System.out.print("#");			// 출력
+		}
+	}
+}
