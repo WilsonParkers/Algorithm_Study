50bff677993f49deb39ff5ffda852251b1e1ac36
seonmin-kim <30610392+seonmin-kim@users.noreply.github.com>
2017-11-03 00:03:01 +0900

    Create 김선민_Samsung_Level1_N까지합.java

---
+++ Algorithm/171021/김선민_Samsung_Level1_N까지합.java
@@ -0,0 +1,11 @@
+package samsung_Level1;
+
+import java.util.Scanner;
+
+public class Samsung_Level1_Nplus {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num*(num+1)/2);		// 1~n까지 합
+	}
+}
