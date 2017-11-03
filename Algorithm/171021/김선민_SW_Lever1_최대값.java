9d766b32538e122e729b2ca7b60a13b1ed61f3ec
seonmin-kim <30610392+seonmin-kim@users.noreply.github.com>
2017-11-03 12:45:03 +0900

    Create 김선민_SW_Lever1_최대값.java

---
+++ Algorithm/171021/김선민_SW_Lever1_최대값.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SW_Lever1_MaxNum {
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("개수 ");
+			int line = Integer.parseInt(sc.nextLine());	// 테스트 개수
+			int num=0;																	// 테스트 수
+
+			for(int i=0; i<line; i++){
+				int max = 0;															// 최대값
+				String result = "#"+ (i+1) +" ";					// 출력 형식
+				for(int j=0; j<10; j++){
+					System.out.print("수 ");
+					num = sc.nextInt();
+					max = max > num ? max : num;						// 최대값 찾기
+				}
+				System.out.println(result + max);
+			}
+	}
+}
